c14029b6693439b574382e726bd0eae19f16b039
Zewu Chen <contact.zewu@gmail.com>
2019-05-28 15:00:50 -0300

    finished bronze exercise

--- CustomView/Controllers/ProgressCircleViewController.swift
+++ CustomView/Controllers/ProgressCircleViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ProgressCircleViewController: UITableViewController {
 
     @IBOutlet weak var progressCircleView: ProgressCircleView?
+    @IBOutlet weak var progressCircleView2: ProgressCircleView!
+    @IBOutlet weak var progressCircleView3: ProgressCircleView!
     @IBOutlet weak var widthSlider: UISlider!
     @IBOutlet weak var progressSlider: UISlider!
     @IBOutlet weak var targetSlider: UISlider!
@@ -21,24 +23,40 @@ class ProgressCircleViewController: UITableViewController {
         self.progressCircleView?.circleWidth = CGFloat(self.widthSlider.value)
         self.progressCircleView?.progressValue = CGFloat(self.progressSlider.value)
         self.progressCircleView?.targetValue = CGFloat(self.targetSlider.value)
+        
+        self.progressCircleView2?.circleWidth = CGFloat(self.widthSlider.value)
+        self.progressCircleView2?.progressValue = CGFloat(self.progressSlider.value)
+        self.progressCircleView2?.targetValue = CGFloat(self.targetSlider.value)
+        
+        self.progressCircleView3?.circleWidth = CGFloat(self.widthSlider.value)
+        self.progressCircleView3?.progressValue = CGFloat(self.progressSlider.value)
+        self.progressCircleView3?.targetValue = CGFloat(self.targetSlider.value)
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         
         self.progressCircleView?.setNeedsDisplay()
+        self.progressCircleView2?.setNeedsDisplay()
+        self.progressCircleView3?.setNeedsDisplay()
     }
 
     @IBAction func widthChange(_ sender: Any) {
         self.progressCircleView?.circleWidth = CGFloat(self.widthSlider.value)
+        self.progressCircleView2?.circleWidth = CGFloat(self.widthSlider.value)
+        self.progressCircleView3?.circleWidth = CGFloat(self.widthSlider.value)
     }
     
     @IBAction func progessChange(_ sender: Any) {
         self.progressCircleView?.progressValue = CGFloat(self.progressSlider.value)
+        self.progressCircleView2?.progressValue = CGFloat(self.progressSlider.value)
+        self.progressCircleView3?.progressValue = CGFloat(self.progressSlider.value)
     }
     
     @IBAction func targetChange(_ sender: Any) {
         self.progressCircleView?.targetValue = CGFloat(self.targetSlider.value)
+        self.progressCircleView2?.targetValue = CGFloat(self.targetSlider.value)
+        self.progressCircleView3?.targetValue = CGFloat(self.targetSlider.value)
     }
 }
 
